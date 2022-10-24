008344d2e4fc6bb65624874988eb285fde305d1d
Dani-vn18 <juanda.vn18@gmail.com>
2022-10-24 11:27:48 -0500

    Segundo commit

--- src/com/sena/Algoritmo12Daniel.java
+++ src/com/sena/Algoritmo12Daniel.java
@@ -1,1 +1,15 @@
 package com.sena;
+
+import javax.swing.*;
+
+public class Algoritmo12Daniel {
+    public static void main(String[] args) {
+
+        double n1=Double.parseDouble(JOptionPane.showInputDialog("Ingrese un número: "));
+        if((n1>=0 && n1<=10)||(n1>=90 && n1<=100)){
+        }
+        else{
+            JOptionPane.showMessageDialog(null,"El número no es permitido");
+        }
+    }
+}
